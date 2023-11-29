856c9c46adfe5969ef986af3ceaa5e02722e1704
JeronimoHoulin <jeronimo.houlin@gmail.com>
2023-11-29 15:42:57 -0300

    readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Uniswap V3 Arbitrage Bot. 
+
+## Solidity smart contract for flash loan and trade execution + Python for event streams (monitoring Uniswap Pools).
+
+### Earn profit on any token by monitoring imbalanced pool activity.
+
+### Flash loan: Loan TokenA => Arbitrage => Profit - (Pay Back Loan + Fee).
+
+### Arbitrage: TokenA => (Pool1) => TokenB => (Pool2) => TokenA. 
--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 import sys
 sys.path.append('./functions')
-
 from get_whitelist_tokens import get_whitelist_tokens
-#from one_inch import get_quote
 from event_stream import stream_txns
 from uni_v3 import get_pools
 
-
-WETH = '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619'  #WETH
+#The token you want to profit in (have a min of $10 worth for flash loan fees).
+token_in = '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619' #WETH
+#Minimum expected profit for the txn to be executed. 
 min_gain = 0.002
 
 
-#Avoids salmonella tokens easily...
+#Avoids salmonella tokens by fetching whitelist tokens only.
 whitelist_tokens = get_whitelist_tokens()
 
 def loop_tokens(token_in, whitelist_tokens, min_gain):
-
     all_whitelist_pools = []
     print("Fetching all whitelist pools...")
     print()
     for token in whitelist_tokens:
-        pools = get_pools(WETH, token)
+        pools = get_pools(token_in, token)
         for pool in pools:
             all_whitelist_pools.append(pool['id'])
-
-
+    
     stream_txns(all_whitelist_pools, min_gain)
 
 if __name__ == '__main__':
-    loop_tokens(WETH, whitelist_tokens, min_gain)
-    #imbalance_search()
+    loop_tokens(token_in, whitelist_tokens, min_gain)
